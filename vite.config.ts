d744575bf1a3b6361a37b2f029e9efff4d804457
Solorza <david.josue.solorza@gmail.com>
2025-07-11 17:46:41 -0500

    error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 esto es clave para Netlify
+  base: '/', // 👈 importante para que Netlify entienda bien las rutas
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
